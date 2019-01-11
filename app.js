4fe35c3e4cefb411790d807dfcf7a511cae5e582
Pavel Stukov <pavels@superdraft.io>
2019-01-11 16:03:42 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+// App dependents
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/sample-app', {
+    poolSize: 10,
+    ssl: false,
+    autoReconnect: true,
+    useNewUrlParser: true,
+    reconnectTries: 10,
+    reconnectInterval: 10000
+});
+
+mongoose.Promise = global.Promise;
+
+// Route modules
+const indexRouter = require('./routes/index');
+const profileRouter = require('./routes/profile');
+
+const publicDir = path.join(__dirname, 'public');
+const app = express();
+
+app.use(express.json());
+app.use(express.static(publicDir));
+
+// Routes
+app.use('/', indexRouter);
+app.use('/profile', profileRouter);
+
+module.exports = app;
